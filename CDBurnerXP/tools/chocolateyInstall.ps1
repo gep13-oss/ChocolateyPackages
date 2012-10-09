b7da6c6e6c666035377ce25b175eeb4ccb72db4b
Gary Ewan Park <gep13@gep13.co.uk>
2012-10-09 12:58:32 +0100

    CDBurnerXP - changed exe install flag
    Changed the install flag from /quiet to /verysilent in order to force
    installation in quiet mode.

--- CDBurnerXP/tools/chocolateyInstall.ps1
+++ CDBurnerXP/tools/chocolateyInstall.ps1
@@ -2,4 +2,4 @@ $name = 'CDBurnerXP'
 $32BitUrl  = 'http://cdburnerxp.se/downloadsetup.exe'
 $64BitUrl  = 'http://cdburnerxp.se/downloadsetup.exe?x64'
 
-Install-ChocolateyPackage $name 'EXE' '/quiet' $32BitUrl $64BitUrl -validExitCodes @(0)
+Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl $64BitUrl -validExitCodes @(0)
