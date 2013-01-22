fdede5d75a270cdd78da1e1632ba8bd37397dff2
Gary Ewan Park <gep13@gep13.co.uk>
2013-01-22 20:22:45 +0000

    Created a new package based on boxstarter
    In order to configure a blank system as per my own preferences, this
    package will install all the required packages, and setup operating
    system preferences.

---
+++ gep13.boxstarter/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$helperDir = (Get-ChildItem $env:ChocolateyInstall\lib\boxstarter.helpers*)
+if($helperDir.Count -gt 1){$helperDir = $helperDir[-1]}
+import-module $helperDir\boxstarter.helpers.psm1
+
+try {
+	Set-TaskbarSmall
+catch {
+    Write-ChocolateyFailure 'jivkok.BoxStarter1' $($_.Exception.Message)
+    throw 
+}
