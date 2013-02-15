688a86242d19e2348ac074ad136535a3839e933f
Gary Ewan Park <gep13@gep13.co.uk>
2013-02-15 22:15:37 +0000

    Updated the check to see if .Net installed
    Messed up the package.  Replaced with a check of the registry key.

--- DotNet4.0/tools/chocolateyInstall.ps1
+++ DotNet4.0/tools/chocolateyInstall.ps1
@@ -1,11 +1,4 @@
-if(Is64Bit) {
-	$fx="framework64"
-} 
-else {
-	$fx="framework"
-}
-
-if(!(Test-Path "$env:windir\Microsoft.Net\$fx\v4.0.30319")) {
+if(!(Test-Path "hklm:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.0")) {
     $env:chocolateyPackageFolder="$env:temp\chocolatey\webcmd"
     Install-ChocolateyZipPackage 'webcmd' 'http://www.iis.net/community/files/webpi/webpicmdline_anycpu.zip' $env:temp
     .$env:temp\WebpiCmdLine.exe /products: NetFramework4 /accepteula
