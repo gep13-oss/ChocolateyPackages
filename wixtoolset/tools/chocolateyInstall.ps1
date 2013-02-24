5ab371a3ad9c55a8936653e1cf8fd913366856eb
Gary Ewan Park <gep13@gep13.co.uk>
2013-02-24 16:15:43 +0000

    Updated download url
    Changed the download url to be more specific.

--- wixtoolset/tools/chocolateyInstall.ps1
+++ wixtoolset/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name = 'wixtoolset'
-$32BitUrl  = 'http://wix.codeplex.com/downloads/get/582218'
+$32BitUrl  = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=582218&FileTime=130008656354370000&Build=20154'
 $64BitUrl  = ''
 
 Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl -validExitCodes @(0)
