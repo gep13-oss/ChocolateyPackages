f73f5a3746368a09889e3430295a8b9121df8b0d
Gary Ewan Park <gep13@gep13.co.uk>
2013-02-24 15:53:11 +0000

    Creating new wix toolset package
    Added the basic package and icon for the wix toolset installation.

---
+++ wixtoolset/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$name = 'wixtoolset'
+$32BitUrl  = 'http://wix.codeplex.com/downloads/get/582218'
+$64BitUrl  = ''
+
+Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl -validExitCodes @(0)
