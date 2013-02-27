66cd95cd051a72a57ccbba7b2aace54c3077d0c3
Gary Ewan Park <gep13@gep13.co.uk>
2013-02-27 07:28:57 +0000

    Updated install flags
    Based on conversation with Rob, trying to use the /q flag for installing
    the app.

--- wixtoolset/tools/chocolateyInstall.ps1
+++ wixtoolset/tools/chocolateyInstall.ps1
@@ -2,4 +2,4 @@
 $32BitUrl  = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=582218&FileTime=130008656354370000&Build=20154'
 $64BitUrl  = ''
 
-Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl -validExitCodes @(0)
+Install-ChocolateyPackage $name 'EXE' '/q' $32BitUrl -validExitCodes @(0)
