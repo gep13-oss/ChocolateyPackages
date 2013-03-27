ff010928363afb56e6e2fa1d1d7fa828035a8e48
Gary Ewan Park <gep13@gep13.co.uk>
2013-03-27 20:50:52 +0000

    Initial creation of the fiddler4 package
    This is VERY rough, but just saving it here so that I can get to it from
    another machine.

---
+++ fiddler4/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'fiddler4' # arbitrary name for the package, used in messages
+$installerType = 'EXE' #only one of these two: exe or msi
+$url = 'http://www.getfiddler.com/dl/Fiddler4BetaSetup.exe' # download url
+$url64 = $url # 64bit URL here or just use the same as $url
+$silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0,1) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
