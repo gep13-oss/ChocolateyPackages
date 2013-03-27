95ab107978928b3424eb677c5d69d90a3ffacea6
Gary Ewan Park <gep13@gep13.co.uk>
2013-03-27 21:56:14 +0000

    Removed comments
    Removed comments from ps1 file as they are not actually required.

--- fiddler4/tools/chocolateyInstall.ps1
+++ fiddler4/tools/chocolateyInstall.ps1
@@ -1,10 +1,8 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-$packageName = 'fiddler4' # arbitrary name for the package, used in messages
-$installerType = 'EXE' #only one of these two: exe or msi
-$url = 'http://www.getfiddler.com/dl/Fiddler4BetaSetup.exe' # download url
-$url64 = $url # 64bit URL here or just use the same as $url
-$silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0,1) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+﻿$packageName = 'fiddler4'
+$installerType = 'EXE'
+$url = 'http://www.getfiddler.com/dl/Fiddler4BetaSetup.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0,1)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
