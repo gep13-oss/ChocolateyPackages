5031ecee8a21133743d84203feb45875a073fae7
Gary Ewan Park <gep13@gep13.co.uk>
2013-05-17 22:29:10 +0100

    Added uninstall script for Calibre
    I won't be able to use this just now, as I will have to wait until the next release of Calibre, but it is at least there, ready for using.

---
+++ calibre/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+$package = "Calibre"
+$packageWildCard = $package + "*";
+
+try {
+	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageWildCard  -and ($_.Version -eq "0.9.31") }
+	$result = $app.Uninstall();
+	
+	Write-ChocolateySuccess $package
+}
+catch {
+	Write-ChocolateyFailure $package "$($_.Exception.Message)"
+	throw
+}
