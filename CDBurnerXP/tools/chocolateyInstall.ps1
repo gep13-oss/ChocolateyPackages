45468da28f271a6b3159c936c2bfdc0c8b59c64d
TomOne <user.email@fake.com>
2013-07-21 20:32:07 +0200

    bumped cdburnerxp to 4.5.2.4214

--- CDBurnerXP/tools/chocolateyInstall.ps1
+++ CDBurnerXP/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-$name = 'CDBurnerXP'
-$32BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.1.4003.exe'
-$64BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.1.4003_x64.exe'
+﻿$name = 'CDBurnerXP'
+$32BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4214.exe'
+$64BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4214_x64.exe'
 
 Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl $64BitUrl -validExitCodes @(0)
