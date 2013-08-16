bfe52d17f4dd0d7721503a07d68399c98b7d2d2a
Gary Ewan Park <gep13@gep13.co.uk>
2013-08-16 19:54:40 +0100

    Updated to version 0.9.42
    - Checked/updated download urls
    - Updated version number
    - Updated release notes

--- calibre/tools/chocolateyUninstall.ps1
+++ calibre/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $package = "Calibre"
 $packageWildCard = $package + "*";
 
 try {
-	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageWildCard  -and ($_.Version -eq "0.9.41") }
+	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageWildCard  -and ($_.Version -eq "0.9.42") }
 	$result = $app.Uninstall();
 	
 	Write-ChocolateySuccess $package
