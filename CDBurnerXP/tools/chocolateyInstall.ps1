121d27a32779bfa9fe1a36d83672977b33095443
Gary Ewan Park <gep13@gep13.co.uk>
2013-10-06 11:08:12 +0100

    Updated to version 4.5.2.4291
    - Checked/updated download urls
    - Updated version number
    - Updated release notes

--- CDBurnerXP/tools/chocolateyInstall.ps1
+++ CDBurnerXP/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name = 'CDBurnerXP'
-$32BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4255.exe'
-$64BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4255_x64.exe'
+$32BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4291.exe'
+$64BitUrl  = 'http://download.cdburnerxp.se/cdbxp_setup_4.5.2.4291_x64.exe'
 
 Install-ChocolateyPackage $name 'EXE' '/verysilent' $32BitUrl $64BitUrl -validExitCodes @(0)
