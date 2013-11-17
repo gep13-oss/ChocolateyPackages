27464b62fa519c9128b2b5500bf8ff1b09bd18e9
Gary Ewan Park <gep13@gep13.co.uk>
2013-11-17 21:13:12 +0000

    Updated to version 5.8
    - Checked/updated download urls
    - Updated version number
    - Updated release notes
    - Added uninstall script
    - Added 64Bit download url

--- PowerISO/tools/chocolateyInstall.ps1
+++ PowerISO/tools/chocolateyInstall.ps1
@@ -1,4 +1,8 @@
-$name = 'poweriso'
-$url  = 'http://www.poweriso.net/PowerISO5.exe'
+$packageName = 'poweriso'
+$installerType = 'EXE'
+$url  = 'http://192.155.93.226/PowerISO5.exe'
+$url64 = 'http://192.155.93.226/PowerISO5-x64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
 
-Install-ChocolateyPackage $name 'EXE' '/S' $url -validExitCodes @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
