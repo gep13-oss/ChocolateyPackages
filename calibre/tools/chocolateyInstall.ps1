62fbb1cc2b8844c30d7cfd7ad1b0d6561971267d
Gary Ewan Park <gep13@gep13.co.uk>
2013-11-27 15:09:19 +0000

    Updated to version 1.12.0
    - Checked/updated download urls
    - Updated version number
    - Updated release notes

--- calibre/tools/chocolateyInstall.ps1
+++ calibre/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $name = 'calibre'
-$32BitUrl  = 'http://sourceforge.net/projects/calibre/files/1.11.0/calibre-1.11.0.msi/download'
-$64BitUrl  = 'http://sourceforge.net/projects/calibre/files/1.11.0/calibre-64bit-1.11.0.msi/download'
+$32BitUrl  = 'http://sourceforge.net/projects/calibre/files/1.12.0/calibre-1.12.0.msi/download'
+$64BitUrl  = 'http://sourceforge.net/projects/calibre/files/1.12.0/calibre-64bit-1.12.0.msi/download'
 
 Install-ChocolateyPackage $name 'MSI' '/quiet' $32BitUrl $64BitUrl -validExitCodes @(0)
