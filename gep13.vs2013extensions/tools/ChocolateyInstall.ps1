1924cafe0806ea84f362160f98e42f60a7726031
Gary Ewan Park <gep13@gep13.co.uk>
2014-01-17 16:38:06 +0000

    Update ChocolateyInstall.ps1
    Added Web Essentials extension

--- gep13.vs2013extensions/tools/ChocolateyInstall.ps1
+++ gep13.vs2013extensions/tools/ChocolateyInstall.ps1
@@ -1,5 +1,6 @@
 try {
 	Install-ChocoatelyVsixPackage "PowerShell Tools for Visual Studio" "http://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/112013/5/PowerShellTools.vsix"
+	Install-ChocolateyVsixPackage "Web Essentials 2013" "http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/27/WebEssentials2013.vsix"
 
     Write-ChocolateySuccess 'gep13.vs2013extensions'
 } catch {
