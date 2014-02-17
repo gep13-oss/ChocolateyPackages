d1d4059d922fe74c71e9976f6634b542a6ed209a
Gary Ewan Park <gep13@gep13.co.uk>
2014-02-17 11:01:15 +0000

    Update ChocolateyInstall.ps1
    Added Productivity Power Tools extension

--- gep13.vs2010extensions/tools/ChocolateyInstall.ps1
+++ gep13.vs2010extensions/tools/ChocolateyInstall.ps1
@@ -3,6 +3,7 @@ try {
 	Install-ChocolateyVsixPackage "PowerCommands for Visual Studio 2010" "http://visualstudiogallery.msdn.microsoft.com/e5f41ad9-4edc-4912-bca3-91147db95b99/file/7088/6/PowerCommands.vsix"
 	Install-ChocolateyVsixPackage "Visual Studio Spell Checker" "http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/2/VSSpellCheckerPackage.vsix"
 	Install-ChocolateyVsixPackage "Web Essentials" "http://visualstudiogallery.msdn.microsoft.com/6ed4c78f-a23e-49ad-b5fd-369af0c2107f/file/50769/32/WebEssentials.vsix"
+	Install-ChocolateyVsixPackage "Productivity Power Tools" "http://visualstudiogallery.msdn.microsoft.com/d0d33361-18e2-46c0-8ff2-4adea1e34fef/file/29666/12/ProPowerTools.vsix"
 	
     Write-ChocolateySuccess 'gep13.vs2010extensions'
 } catch {
