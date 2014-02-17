01fe0f3fa15e8470237ebb554554780cce189085
Gary Ewan Park <gep13@gep13.co.uk>
2014-02-17 11:15:41 +0000

    Update ChocolateyInstall.ps1
    Corrected spelling mistakes.

--- gep13.vs2010extensions/tools/ChocolateyInstall.ps1
+++ gep13.vs2010extensions/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ try {
 	Install-ChocolateyVsixPackage "Visual Studio Spell Checker" "http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/2/VSSpellCheckerPackage.vsix"
 	Install-ChocolateyVsixPackage "Web Essentials" "http://visualstudiogallery.msdn.microsoft.com/6ed4c78f-a23e-49ad-b5fd-369af0c2107f/file/50769/32/WebEssentials.vsix"
 	Install-ChocolateyVsixPackage "Productivity Power Tools" "http://visualstudiogallery.msdn.microsoft.com/d0d33361-18e2-46c0-8ff2-4adea1e34fef/file/29666/12/ProPowerTools.vsix"
-	Install-ChocoatelyVsixPackage "StopOnFirstBuildError" "http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix"
+	Install-ChocolateyVsixPackage "StopOnFirstBuildError" "http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix"
 	
 	Write-ChocolateySuccess 'gep13.vs2010extensions'
 } catch {
