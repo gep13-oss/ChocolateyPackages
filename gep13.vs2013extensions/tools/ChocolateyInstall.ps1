ac07943c1546c55084997954dfbfb73c52171b24
Gary Ewan Park <gep13@gep13.co.uk>
2014-02-17 11:04:04 +0000

    Update ChocolateyInstall.ps1
    Added StopOnFirstBuildError Extension

--- gep13.vs2013extensions/tools/ChocolateyInstall.ps1
+++ gep13.vs2013extensions/tools/ChocolateyInstall.ps1
@@ -3,7 +3,7 @@ try {
 	Install-ChocolateyVsixPackage "Web Essentials 2013" "http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/27/WebEssentials2013.vsix"
 	Install-ChocoatelyVsixPackage "Visual Studio Spell Checker" "http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/2/VSSpellCheckerPackage.vsix"
 	Install-ChocoatelyVsixPackage "Productivity Power Tools 2013" "http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/3/ProPowerTools.vsix"
-
+	Install-ChocoatelyVsixPackage "StopOnFirstBuildError" "http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix"
 
 	Write-ChocolateySuccess 'gep13.vs2013extensions'
 } catch {
