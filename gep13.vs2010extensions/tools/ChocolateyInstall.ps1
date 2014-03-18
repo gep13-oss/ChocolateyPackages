efd58c9c5a136cb6bcf423efa3b228a798ac643d
Gary Ewan Park <gep13@gep13.co.uk>
2014-03-18 20:48:12 +0000

    Added CodeMaid extension
    Following recommendation here:
    
    http://channel9.msdn.com/coding4fun/blog/Need-help-cleaning-up-your-code-CodeMaid-to-the-rescue

--- gep13.vs2010extensions/tools/ChocolateyInstall.ps1
+++ gep13.vs2010extensions/tools/ChocolateyInstall.ps1
@@ -6,6 +6,7 @@ try {
 	Install-ChocolateyVsixPackage "Productivity Power Tools" "http://visualstudiogallery.msdn.microsoft.com/d0d33361-18e2-46c0-8ff2-4adea1e34fef/file/29666/12/ProPowerTools.vsix"
 	Install-ChocolateyVsixPackage "StopOnFirstBuildError" "http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix"
 	Install-ChocolateyVsixPackage "Ref12" "http://visualstudiogallery.msdn.microsoft.com/f89b27c5-7d7b-4059-adde-7ccc709fa86e/file/125016/4/Ref12.vsix"
+	Install-ChocolateyVsixPackage "CodeMaid" "http://visualstudiogallery.msdn.microsoft.com/76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/23/CodeMaid_v0.7.0.vsix"
 	
 	Write-ChocolateySuccess 'gep13.vs2010extensions'
 } catch {
