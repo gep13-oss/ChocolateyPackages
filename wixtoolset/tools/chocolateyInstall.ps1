89c6b87ff84e64116c6c8b271fece64fe45cd2fa
Gary Ewan Park <gep13@gep13.co.uk>
2014-11-05 21:27:18 +0000

    (wixtoolset) First pass at 3.9 package

--- wixtoolset/tools/chocolateyInstall.ps1
+++ wixtoolset/tools/chocolateyInstall.ps1
@@ -1,5 +1,7 @@
-﻿$name = 'wixtoolset'
-$32BitUrl = 'https://wix.codeplex.com/downloads/get/762937'
-$64BitUrl  = ''
+﻿$packageName = 'wixtoolset'
+$installerType = 'EXE'
+$32BitUrl = 'https://wix.codeplex.com/downloads/get/925661'
+$silentArgs = '/q'
+$validExitCodes = @(0)
 
-Install-ChocolateyPackage $name 'EXE' '/q' $32BitUrl -validExitCodes @(0)
+Install-ChocolateyPackage $packageName $installerType '/q' $32BitUrl -validExitCodes $validExitCodes
