40b9a98aca871a527e52a2211413f4a1bcfb8620
Gary Ewan Park <gary.park@dai.co.uk>
2014-11-06 20:25:40 +0000

    (wixtoolet) Moved package to core team repo

--- wixtoolset/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'wixtoolset'
-$installerType = 'EXE'
-$32BitUrl = 'https://wix.codeplex.com/downloads/get/925661'
-$silentArgs = '/q'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage $packageName $installerType '/q' $32BitUrl -validExitCodes $validExitCodes
--- wixtoolset/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = 'wixtoolset'
-$uninstallRegKey = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
-$silentArgs = '/uninstall /quiet'
-
-$uninstallPath = (Get-ItemProperty $uninstallRegKey).BundleCachePath
-
-Start-ChocolateyProcessAsAdmin -exeToRun $uninstallPath -statements $silentArgs
