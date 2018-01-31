aa73080e5574e170e0ebffe14e3a48202263e386
Gary Ewan Park <gep13@gep13.co.uk>
2018-01-31 17:50:29 -0600

    Update ChocolateyInstall.ps1

--- gep13.gitConfig/tools/ChocolateyInstall.ps1
+++ gep13.gitConfig/tools/ChocolateyInstall.ps1
@@ -1,7 +1,8 @@
 git config --global "user.name" "Gary Ewan Park"
 git config --global "user.email" "gep13@gep13.co.uk"
+git config --global user.signingkey 5BE9DB14
 
-git config --global "push.default" "simple"
+git config --global "push.default" "matching"
 
 git config --global "diff.tool" "vscode"
 git config --global "difftool.vscode.cmd" "code --wait --diff $LOCAL $REMOTE"
@@ -9,7 +10,6 @@ git config --global "difftool.vscode.cmd" "code --wait --diff $LOCAL $REMOTE"
 git config --global "merge.ff" "false"
 git config --global "merge.log" "true"
 git config --global "merge.renamelimit" "6500"
-
 git config --global "merge.tool" "kdiff3"
 
 # Windows
@@ -27,7 +27,6 @@ git config --global "core.symlinks" "false"
 git config --global "core.autocrlf" "false"
 git config --global "core.editor" "code --wait"
 
-git config --global user.signingkey 5BE9DB14
 git config --global commit.gpgsign true
 
 # Windows
@@ -35,3 +34,13 @@ git config --global "gpg.program" "C:/Program Files (x86)/GNU/GnuPG/pub/gpg.exe"
 
 # Mac
 # On a Mac, the path to the gpg program isn't required, as this is already on the path
+
+git config --global "alias.st" "status"
+git config --global "alias.standup" "!git log --since yesterday --oneline --author 'gep13@gep13.co.uk'"
+git config --global "alias.lg" "log --pretty=format:'%C(yellow)%h%C(reset) %s %C(cyan)%cr%C(reset) %C(blue)%an%C(reset) %C(green)%d%C(reset)' --graph --date-order"
+git config --global "alias.review" "log -p --reverse -M -C -C --patience --no-prefix"
+
+git config --global "color.branch" "auto"
+git config --global "color.diff" "auto"
+git config --global "color.status" "auto"
+git config --global "color.ui" "auto"
